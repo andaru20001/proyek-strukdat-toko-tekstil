33c0c4291643280c7a4ca2765e3e407b019ac956
muaz <muaz20001@mail.unpad.ac.id>
2021-05-08 09:54:26 +0700

    membuat menu dari fungsi yang sudah dibuat

--- main.cpp
+++ main.cpp
@@ -15,28 +15,57 @@ int main(){
 
     // Backup data yang sudah disimpan,
 
+    bool ulang = true;
+    while(ulang){
+        std::cout << "PROGRAM SISTEM ANTRIAN PEMESANAN TOKO TEKSTIL\n";
+        std::cout << "1. Tambah Pesanan\n2. Hapus Pesanan\n3. Search\n 4. Daftar Pesanan\n5. Edit Pesanan\n6. Undo\n7. Save & Exit\n8. Exit without Saving\n";
 
-    // Tampilkan kode-kode untuk masuk ke salah satu programnya. 
-    // Sesuaikan saja dengan urutan yang ada di README.md :
-    // 1 untuk program tambah
-    // 2 untuk program hapus
-    // 3 untuk program search
-    // 4 untuk program daftar pesanan
-    // 5 untuk program edit pesanan
-    // 6 untuk undo
-    std::cin >> kodeProgram;
-    
-    // Jangan lupa sediakan opsi save dan exit dan exit without saving (mungkin bisa diletakkan di pilihan menu kode 7 dan 8)
-    // Exit wihout saving cuma untuk error handling ketika pengguna mau membatalkan semua perubahan
-    // Untuk variabel kode untuk pilih programnya disepakati aja kali ya, jadi "kodeProgram"
-
-    switch (kodeProgram){
-        case 1:
-            Pointer PesananBaru = new Pesanan;
-            buatPesanan(PesananBaru);
-            isi_data(Q, PesananBaru);
-            tambah(Q, PesananBaru);
-            catat_log(S, PesananBaru, 1);
-            break;
+        // Tampilkan kode-kode untuk masuk ke salah satu programnya. 
+        // Sesuaikan saja dengan urutan yang ada di README.md :
+        // 1 untuk program tambah
+        // 2 untuk program hapus
+        // 3 untuk program search
+        // 4 untuk program daftar pesanan
+        // 5 untuk program edit pesanan
+        // 6 untuk undo
+        std::cout << "Masukan Pilihan   :\n";
+        std::cin >> kodeProgram;
+        
+        // Jangan lupa sediakan opsi save dan exit dan exit without saving (mungkin bisa diletakkan di pilihan menu kode 7 dan 8)
+        // Exit wihout saving cuma untuk error handling ketika pengguna mau membatalkan semua perubahan
+        // Untuk variabel kode untuk pilih programnya disepakati aja kali ya, jadi "kodeProgram"
+        std::string search;
+        Pointer prev, temp;
+        switch (kodeProgram){
+            case 1:
+                Pointer PesananBaru = new Pesanan;
+                buatPesanan(PesananBaru);
+                isi_data(Q, PesananBaru);
+                tambah(Q, PesananBaru);
+                catat_log(S, PesananBaru, 1);
+                break;
+            case 2:
+                std::cout << "Masukan Kode Pesanan  : ";
+                std::cin >> search;
+                hapusPesanan(Q, search);
+                break;
+            case 3:
+                std::cout << "Masukan Kode Pesanan  : ";
+                std::cin >> search;
+                find(Q, prev, search, temp);
+                if(temp->kode == search){
+                    std::cout << temp->kode << '\n';
+                    std::cout << temp->deskripsi << '\n';
+                    std::cout << "Tanggal   : " << temp->tglPesan << '\n';
+                    std::cout << "Tenggat   : " << temp->tenggat << '\n';
+                }
+                break;
+            case 7:
+                ulang = false;
+                break;
+            case 8:
+                ulang = false;
+                break;
+        }
     }
 }
