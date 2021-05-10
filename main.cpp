f907f0f676f668b4432b3c2e136aca8ffd993feb
DaffaAlif <daffalif02@gmail.com>
2021-05-10 13:28:21 +0700

    ngerapihin case sesuai dengan menu

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(){
     bool ulang = true;
     while(ulang){
         std::cout << "PROGRAM SISTEM ANTRIAN PEMESANAN TOKO TEKSTIL\n";
-        std::cout << "1. Tambah Pesanan\n2. Hapus Pesanan\n3. Search\n 4. Daftar Pesanan\n5. Edit Pesanan\n6. Undo\n7. Save & Exit\n8. Exit without Saving\n";
+        std::cout << "1. Tambah Pesanan\n2. Hapus Pesanan\n3. Search\n4. Daftar Pesanan\n5. Edit Pesanan\n6. Undo\n7. Save & Exit\n8. Exit without Saving\n";
 
         // Tampilkan kode-kode untuk masuk ke salah satu programnya. 
         // Sesuaikan saja dengan urutan yang ada di README.md :
@@ -37,19 +37,27 @@ int main(){
         std::string search;
         Pointer prev, temp;
         switch (kodeProgram){
-            case 1: //Case Tambah Pesanan
+            case 1: {//Case Tambah Pesanan
                 Pointer PesananBaru = new Pesanan;
                 buatPesanan(PesananBaru);
                 isi_data(Q, PesananBaru, 1);
                 tambah(Q, PesananBaru);
                 catat_log(S, PesananBaru, 1);
-                break;
-            case 2: //Case Hapus Pesanan
+                break;}
+            case 2: {//Case Hapus Pesanan
                 std::cout << "Masukan Kode Pesanan  : ";
                 std::cin >> search;
                 hapusPesanan(Q, search);
-                break;
-            case 3: //Case Edit Pesanan
+                break;}
+            case 3: {//case search (display value dari pesanan yang ingin dicari)
+                std::cout << "Masukan Kode Pesanan  : ";
+                std::cin >> search;
+                Search(Q, search);
+                break;}
+            case 4:  {//case daftar pesanan
+                daftarPesanan(Q);
+                break;}
+            case 5:  {//Case Edit Pesanan
                 std::cout << "Masukan Kode Pesanan  : ";
                 std::cin >> search;
                 find(Q, prev, search, temp);
@@ -60,18 +68,11 @@ int main(){
                     // std::cout << "Tenggat   : " << temp->tenggat << '\n';
                     isi_data(Q, temp, 2);
                 }
-                break;
-            case 4:  //case daftar pesanan
-                daftarPesanan(Q);
-                break;
-            case 5:  //case search (display value dari pesanan yang ingin dicari)
-                std::cout << "Masukan Kode Pesanan  : ";
-                std::cin >> search;
-                Search(Q, search);
-                break;
-            case 6:
+                break;}
+            case 6:{
                 ulang = false;
-                break;
+                break;}
         }
     }
 }
+
